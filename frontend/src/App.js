ca1ab904d18252d97be002ae3c9e875aab040810
SANKETH-CHANDRA-SHETTY <shettysankethshetty1@gmail.com>
2025-12-16 01:46:46 +0530

    Import routes protection code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Navigate } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import {useState,useEffect } from "react";
-import userContext from './context/userContext'
+import userContext from './context/userContext';
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   const [user, setUser] = useState(() => {
@@ -24,10 +25,13 @@ function App() {
     <div>
         <userContext.Provider value={{ user, setUser }}>
           <Routes>
-            <Route path="/" element={< Login/>} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/home" element={<Home/>} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
+
+            <Route path="/" element={<Navigate to="/login" />} />
           </Routes>
         </userContext.Provider>
     </div>
