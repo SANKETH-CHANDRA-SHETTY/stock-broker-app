a4b82d0b437d2d2fabe4395cb0eedce0452e31e6
SANKETH-CHANDRA-SHETTY <shettysankethshetty1@gmail.com>
2025-12-16 00:52:53 +0530

    Update backend production url

---
+++ frontend/src/config.js
@@ -0,0 +1,7 @@
+const PROD_URL = "stock-broker-app-production.up.railway.app"; 
+
+const API_URL = window.location.hostname === "localhost" 
+  ? "http://localhost:5000" 
+  : PROD_URL;
+
+export default API_URL;
